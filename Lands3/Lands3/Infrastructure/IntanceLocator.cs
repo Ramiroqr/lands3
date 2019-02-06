c701bf80aa79be7cfc97ff31f31b4731b421347e
Ramiroqr <ramiroquisper@gmail.com>
2019-02-06 12:54:19 -0500

    LoginPage

---
+++ Lands3/Lands3/Infrastructure/IntanceLocator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lands3.Infrastructure
+{
+    using ViewModels;
+    public class IntanceLocator
+    {
+        #region Properties
+        public MainViewModel Main { get; set; }
+        #endregion
+
+        #region Constryctors
+        public IntanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
