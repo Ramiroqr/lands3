11424293b55e7c48c4c44c034fa4b5b285a8ab99
Ramiroqr <ramiroquisper@gmail.com>
2019-02-07 22:32:01 -0500

    LoginViewModels

--- Lands3/Lands3/ViewModels/LoginViewModel.cs
+++ Lands3/Lands3/ViewModels/LoginViewModel.cs
@@ -2,7 +2,11 @@
 
 namespace Lands3.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
+    using System;
     using System.Windows.Input;
+    using Xamarin.Forms;
+
     class LoginViewModel
     {
         #region Properties
@@ -13,11 +17,37 @@ namespace Lands3.ViewModels
         #endregion
 
         #region Commands
-            public ICommand LoginCommand { get; set; }
+            public ICommand LoginCommand
+            {
+                get
+                {
+                    return new RelayCommand(Login);
+                }
+            }
+
+            private async void Login()
+            {
+                if(string.IsNullOrEmpty(this.Email))
+                {
+                    await Application.Current.MainPage.DisplayAlert(
+                        "Error", 
+                        "You must enter an email.", 
+                        "Accept");
+                    return;
+                }
+                if (string.IsNullOrEmpty(this.Password))
+                {
+                    await Application.Current.MainPage.DisplayAlert(
+                        "Error",
+                        "You must enter an password.",
+                        "Accept");
+                    return;
+                }
+        }
         #endregion
 
         #region Constructors
-            public LoginViewModel()
+        public LoginViewModel()
             {
                 this.IsRemembered = true;
             }
