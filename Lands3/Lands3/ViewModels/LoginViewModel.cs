cfa104154c167d525573c6bdf91ab4e84a058e96
Ramiroqr <ramiroquisper@gmail.com>
2019-02-10 10:42:55 -0500

    LoginViewModel y BaseViewModel

--- Lands3/Lands3/ViewModels/LoginViewModel.cs
+++ Lands3/Lands3/ViewModels/LoginViewModel.cs
@@ -3,21 +3,41 @@
 namespace Lands3.ViewModels
 {
     using GalaSoft.MvvmLight.Command;
-    using System;
     using System.Windows.Input;
     using Xamarin.Forms;
 
-    class LoginViewModel
+    class LoginViewModel : BaseViewModel
     {
+        #region Attributes
+            //solo para los campos que necesitan ser actualizados
+            private string password;
+            private bool isRunning;
+            private bool isEnabled;
+        #endregion
         #region Properties
             public string Email { get; set; }
-            public string Password { get; set; }
-            public bool IsRunning { get; set; }
+            public string Password
+            {
+                get { return this.password; }
+                set { SetValue(ref this.password, value); }
+            }
+
+            public bool IsRunning
+            {
+                get { return this.isRunning; }
+                set { SetValue(ref this.isRunning, value); }
+            }
             public bool IsRemembered { get; set; }
+
+            public bool IsEnabled
+            {
+                get { return this.isEnabled; }
+                set { SetValue(ref this.isEnabled, value); }
+            }
         #endregion
 
         #region Commands
-            public ICommand LoginCommand
+        public ICommand LoginCommand
             {
                 get
                 {
@@ -43,6 +63,29 @@ namespace Lands3.ViewModels
                         "Accept");
                     return;
                 }
+
+                this.IsRunning = true;
+                this.IsEnabled = false;
+
+                if(this.Email != "ram@g.com" || this.Password != "123")
+                {
+                    this.IsRunning = false;
+                    this.IsEnabled = true;
+                    await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Email or password incorrect",
+                    "Accept");
+                    this.Password = string.Empty;
+                    return;
+                }
+
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                    "Ok",
+                    "Yeahhh!!!",
+                    "Accept");
+
         }
         #endregion
 
@@ -50,6 +93,7 @@ namespace Lands3.ViewModels
         public LoginViewModel()
             {
                 this.IsRemembered = true;
+                this.IsEnabled = true;
             }
         #endregion
     }
